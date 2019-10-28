dc8cf4dcfa663f0db59cdbc086b4833e985a6405
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 04:58:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,6 +33,11 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
+def using_insert(array, element)
+   array = ["great", "good", "nada", "sleepy"]
+   element = "really sleepy"
+   array.insert(element)
+end
 
 
   
