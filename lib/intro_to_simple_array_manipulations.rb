6b2c27a33572b7e30d6db747634f1207ca9f4056
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 04:45:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(array, string)
-  array = ["apple", "dog","cat"]
-  string = "puppy"
-  array.push(string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green","blue", "indigo"]
+    next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
 end
